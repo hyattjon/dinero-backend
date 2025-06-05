b25ec16e9587e49e4b3da30390cf99a8c10faf22
Jonathan Hyatt <hyatt.jonathan99@gmail.com>
2025-06-05 10:58:46 -0600

    functional on local and outside deployment

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import random
 
 load_dotenv()
 
-# This version works
+# This version works as well
 # Add Plaid configuration
 PLAID_CLIENT_ID = os.environ.get('PLAID_CLIENT_ID')
 PLAID_SECRET = os.environ.get('PLAID_SECRET')
