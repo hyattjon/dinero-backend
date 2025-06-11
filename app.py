91a8f7bdcbcfe1228d5abe540ad00a15354b0916
Jonathan Hyatt <hyatt.jonathan99@gmail.com>
2025-06-11 08:15:18 -0500

    this is a functional login version

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ from functools import wraps
 from supabase import create_client, Client
 import uuid
 
-# This works now
+# This works now again
 # Environment variables and configuration
 load_dotenv()
 
